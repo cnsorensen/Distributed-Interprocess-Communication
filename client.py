f34945cdc8783f87156e27bf0d0377ff568b4b86
cnsorensen <christine.sorensen@mines.sdsmt.edu>
2016-03-27 10:14:31 -0600

    Initial commit with the code provided by McGough

---
+++ client.py
@@ -0,0 +1,14 @@
+from socket import *
+HOST = 'localhost'
+PORT = 50007
+s = socket ( AF_INET , SOCK_STREAM ) 
+s.connect((HOST, PORT))
+while True :
+  message = raw_input("> ") 
+  if message == 'q': break
+  if message != '': 
+    s.send(message)
+    data = s.recv(1024)
+    print 'Received' , repr(data)
+s.close()
+
