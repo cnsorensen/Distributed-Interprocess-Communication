d5004ac8230e193f3e654df4adde39948d368d2d
cnsorensen <christine.sorensen@mines.sdsmt.edu>
2016-04-03 16:56:14 -0600

    So this repo is a bit of a mess. I'm working on multiserver.c in the csockets directory. I'm using chat_client.py to test it. From what I understand, we don't need to really write our own client maybs, not sure. I'm just working on multiserver.c. Thought I should update.

--- csockets/multiserver.c
+++ csockets/multiserver.c
@@ -4,19 +4,59 @@
     http://www.binarytides.com/server-client-example-c-sockets-linux/
 */
  
-#include<stdio.h>
-#include<string.h>    //strlen
-#include<stdlib.h>    //strlen
-#include<sys/socket.h>
-#include<arpa/inet.h> //inet_addr
-#include<unistd.h>    //write
-#include<pthread.h> //for threading , link with lpthread
+#include <stdio.h>
+#include <string.h>    //strlen
+#include <stdlib.h>    //strlen
+#include <sys/socket.h>
+#include <arpa/inet.h> //inet_addr
+#include <unistd.h>    //write
+#include <pthread.h> //for threading , link with lpthread
  
-//the thread function
-void *connection_handler(void *);
- 
+// Fucntion prototypes
+void* connection_handler( void* );
+int message_handler( char* );
+char* remove_newline( char* );
+
+// Parameters passed in
+int NUM_MAILBOX = 0;
+int MAILBOX_SIZE = 0;
+int PORT = 8888;
+int PACKET_SIZE = 0;
+char** MAILBOX;
+
+
 int main( int argc, char** argv )
 {
+    // check for usage
+    if( argc != 5 )
+    {
+        printf( "Usage: dipc <num mailboxes> <mailbox sixe KB> <port num> <size of packets>\n");
+        return -1;
+    }
+    
+    // assign the parameter values
+    NUM_MAILBOX = atoi( argv[1] );
+    MAILBOX_SIZE = atoi( argv[2] );
+    PORT = atoi( argv[3] );
+    PACKET_SIZE = atoi( argv[4] );
+
+    // allocate the mailboxes
+    ///figure out deallocation later
+    if( ( MAILBOX = malloc( NUM_MAILBOX * sizeof( char* ) ) ) == NULL )
+    {
+        printf( "Error allocating mailboxes!\n" );
+        return -1;
+    }
+    int i;
+    for( i = 0; i < NUM_MAILBOX; i++ )
+    {
+        if( ( MAILBOX[i] = malloc( MAILBOX_SIZE * sizeof( char* ) ) ) == NULL )
+        {
+            printf( "Error allocating mailboxes!\n" );
+            return -1;
+        }
+    }
+
     int socket_desc, client_sock, c;
     int* new_sock;
     struct sockaddr_in server, client;
@@ -38,10 +78,10 @@ int main( int argc, char** argv )
     if( bind( socket_desc, (struct sockaddr *)&server , sizeof( server ) ) < 0 )
     {
         //print the error message
-        perror( "bind failed. Error" );
+        perror( "Bind failed. Error!" );
         return 1;
     }
-    puts( "bind done" );
+    puts( "Bind done!" );
      
     //Listen
     listen( socket_desc , 3 );
@@ -49,13 +89,9 @@ int main( int argc, char** argv )
     //Accept and incoming connection
     puts( "Waiting for incoming connections..." );
     c = sizeof( struct sockaddr_in );
-     
-    //Accept and incoming connection
-    puts( "Waiting for incoming connections..." );
-    c = sizeof( struct sockaddr_in );
     while( ( client_sock = accept( socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
     {
-        puts( "Connection accepted" );
+        puts( "Connection accepted!" );
          
         pthread_t sniffer_thread;
         new_sock = malloc(1);
@@ -63,19 +99,22 @@ int main( int argc, char** argv )
          
         if( pthread_create( &sniffer_thread , NULL ,  connection_handler , (void*) new_sock) < 0)
         {
-            perror( "Could not create thread" );
-            return 1;
+            perror( "Could not create thread." );
+            return -1;
         }
          
         //Now join the thread , so that we dont terminate before the thread
         //pthread_join( sniffer_thread , NULL);
-        puts( "Handler assigned" );
+        puts( "Handler assigned!" );
     }
+
+    ///huh?
+    printf( "Do I ever leave thsi loop?\n" );
      
     if( client_sock < 0 )
     {
-        perror( "Accept failed" );
-        return 1;
+        perror( "Accept failed." );
+        return -1;
     }
      
     return 0;
@@ -94,16 +133,29 @@ void* connection_handler( void* socket_desc )
      
     //Send some messages to the client
     message = "Greetings! I am your connection handler\n";
-    write( sock , message , strlen( message ) );
-     
+    write( sock , message , strlen( message ) );     
     message = "Now type something and i shall repeat what you type \n";
     write( sock , message , strlen( message ) );
      
-    //Receive a message from client
+    printf( "Socket: %d\n", sock );
+    int flag = 1;
+
+    // Receive a message from client
+    // This continues looping until the client shuts down
     while( ( read_size = recv( sock , client_message , 2000 , 0 ) ) > 0 )
     {
-        //Send the message back to client
-        write( sock , client_message , strlen( client_message ) );
+        // Deal with the message here
+        //printf( "Client's message %s", client_message );
+        flag = message_handler( client_message );
+        // Send the message back to client
+        //write( sock , client_message , strlen( client_message ) );
+        
+        // if the user is quiting
+        if( flag != 1 )
+        {
+            read_size = 0;
+            break;
+        }
     }
      
     if( read_size == 0 )
@@ -111,6 +163,7 @@ void* connection_handler( void* socket_desc )
         puts( "Client disconnected" );
         fflush( stdout );
     }
+
     else if( read_size == -1 )
     {
         perror( "Recv failed" );
@@ -120,4 +173,53 @@ void* connection_handler( void* socket_desc )
     free( socket_desc );
      
     return 0;
+}
+
+int message_handler( char* msg )
+{
+    printf( "Client's message: %s", msg );
+   
+    //msg = remove_newline( msg );
+ 
+    // parse the message
+    const char delim[4] = " \t\n";
+    char* token = NULL;
+    char* tokens[64];
+    int i = 0;
+
+    // extract the first word
+    token = strtok( msg, delim );
+    printf( "token: %s\n", token );
+    //fflush( stdout );
+
+    // check to see if they're calling it quits
+    if( strcmp( token, "q" ) == 0 )
+    {
+        return 0;
+    }
+
+/*
+    // collect the words
+    while( token != NULL )
+    {
+        tokens[i] = token;
+        printf( "%s\n", tokens[i] );
+        i++;
+        token = strtok( NULL, delim );
+    }
+*/ 
+    //token = NULL;
+ 
+    return 1;
+}
+
+char* remove_newline( char* line )
+{
+    size_t len = strlen( line );
+    if( len > 0 && line[len-1] == '\n' )
+    {
+        line[--len] = '\0';
+    }
+
+    return line;
 }
