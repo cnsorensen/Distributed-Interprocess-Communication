e8bd71b09467e28d8d6ec7b45e30d94f87c0ca44
cnsorensen <christine.sorensen@mines.sdsmt.edu>
2016-04-04 18:57:55 -0600

    I have threads up and working. Fixed a bug with writing nothing to a box. Need to write program to shut down the server. Also need to make the program run daemon. Also need to test threads more throughly.

--- csockets/dipc.c
+++ csockets/dipc.c
@@ -5,19 +5,19 @@
 */
  
 #include <stdio.h>
-#include <string.h>    //strlen
-#include <stdlib.h>    //strlen
+#include <string.h>    // strlen
+#include <stdlib.h>    // strlen
 #include <sys/socket.h>
-#include <arpa/inet.h> //inet_addr
-#include <unistd.h>    //write
-#include <pthread.h> //for threading , link with lpthread
+#include <arpa/inet.h> // inet_addr
+#include <unistd.h>    // write
+#include <pthread.h> // for threading , link with lpthread
  
 // Fucntion prototypes
 void* connection_handler( void* );
 int message_handler( char*, int );
 char* remove_newline( char* );
 int read_handler( char*, int );
-int write_handler( char*, char* );
+int write_handler( char*, char*, int );
 int isInt( char* );
 
 // Parameters passed in
@@ -25,7 +25,11 @@ int NUM_MAILBOX = 0;
 int MAILBOX_SIZE = 0;
 int PORT = 8888;
 int PACKET_SIZE = 0;
+
+// THE mailbox
 char** MAILBOX;
+pthread_mutex_t* MAILBOX_LOCK;
+
 
 
 int main( int argc, char** argv )
@@ -33,7 +37,7 @@ int main( int argc, char** argv )
     // check for usage
     if( argc != 5 )
     {
-        printf( "Usage: dipc <num mailboxes> <mailbox sixe KB> <port num> <size of packets>\n");
+        printf( "Usage: dipc <num mailboxes> <mailbox size KB> <port num> <size of packets>\n");
         return -1;
     }
     
@@ -60,41 +64,56 @@ int main( int argc, char** argv )
         }
     }
 
+    // allocate the locks for the mailboxes
+    if( ( MAILBOX_LOCK = malloc( NUM_MAILBOX * sizeof( pthread_mutex_t ) ) ) == NULL )
+    {
+        printf( "Error allocating locks for the mailboxes!\n" );
+        return -1;
+    }
+
+    // initialize the locks
+    ///need to destroy sometime
+    for( i = 0; i < NUM_MAILBOX; i++ )
+    {
+        printf( "Ham\n" );
+        pthread_mutex_init( &MAILBOX_LOCK[i], NULL );
+    }
+
     int socket_desc, client_sock, c;
     int* new_sock;
     struct sockaddr_in server, client;
      
-    //Create socket
+    // Create socket
     socket_desc = socket( AF_INET, SOCK_STREAM, 0 );
     if ( socket_desc == -1 )
     {
         printf( "Could not create socket" );
     }
-    puts( "Socket created" );
+    ///puts( "Socket created" );
      
-    //Prepare the sockaddr_in structure
+    // Prepare the sockaddr_in structure
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
     server.sin_port = htons( 8888 );
      
-    //Bind
+    // Bind
     if( bind( socket_desc, (struct sockaddr *)&server , sizeof( server ) ) < 0 )
     {
-        //print the error message
+        // print the error message
         perror( "Bind failed. Error!" );
-        return 1;
+        return -1;
     }
-    puts( "Bind done!" );
+    ///puts( "Bind done!" );
      
-    //Listen
+    // Listen
     listen( socket_desc , 3 );
 
-    //Accept and incoming connection
+    // Accept and incoming connection
     puts( "Waiting for incoming connections..." );
     c = sizeof( struct sockaddr_in );
     while( ( client_sock = accept( socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
     {
-        puts( "Connection accepted!" );
+        ///puts( "Connection accepted!" );
          
         pthread_t sniffer_thread;
         new_sock = malloc(1);
@@ -106,13 +125,10 @@ int main( int argc, char** argv )
             return -1;
         }
          
-        //Now join the thread , so that we dont terminate before the thread
+        // Now join the thread , so that we dont terminate before the thread
         //pthread_join( sniffer_thread , NULL);
         puts( "Handler assigned!" );
     }
-
-    ///huh?
-    printf( "Do I ever leave thsi loop?\n" );
      
     if( client_sock < 0 )
     {
@@ -133,14 +149,7 @@ void* connection_handler( void* socket_desc )
     int read_size;
     char* message; 
     char client_message[2000];
-     
-    //Send some messages to the client
-    message = "Greetings! I am your connection handler\n";
-    write( sock , message , strlen( message ) );     
-    message = "Now type something and i shall repeat what you type \n";
-    write( sock , message , strlen( message ) );
-     
-    printf( "Socket: %d\n", sock );
+
     int flag = 1;
 
     // Receive a message from client
@@ -172,7 +181,7 @@ void* connection_handler( void* socket_desc )
         perror( "Recv failed" );
     }
          
-    //Free the socket pointer
+    // Free the socket pointer
     free( socket_desc );
      
     return 0;
@@ -205,7 +214,7 @@ int message_handler( char* msg, int sock )
     else if( strcmp( token, "w" ) == 0 )
     {
         printf( "Writing...\n"); 
-        write_handler( write_msg, box_char );
+        write_handler( write_msg, box_char, sock );
     }
 
     // if we're reading from a mailbox
@@ -232,23 +241,32 @@ int read_handler( char* box_char, int sock )
     // holds the message in the box to be printed to the client
     char* msg;
 
+    // error statement if the user provides an invalid number
+    char* num_error = "Invalid mailbox number.\n";
+
     // If the number given from the user isn't even a number at all... idiot
     if( !isInt( box_char ) )
     {
-        printf( "Invalid mailbox number!\n" );
+        write( sock, num_error, strlen( num_error ) );
         return 1;
     }
 
     // convert the given number to an int
     box_num = atoi( box_char );
+    
+    // convert for csc friendly array accessing :)
+    box_num = box_num - 1;
 
     // check to see if it's actually a mailbox
-    if( box_num < 0 || box_num > NUM_MAILBOX )
+    if( box_num < 0 || box_num >= NUM_MAILBOX )
     {
-        printf( "Mailbox does not exist!\n" );
+        write( sock, num_error, strlen( num_error ) );
         return 1;
     }
 
+    // lock the mutex lock to THAT mailbox
+    pthread_mutex_lock( &MAILBOX_LOCK[box_num] );
+    
     // grab that message
     msg = MAILBOX[box_num];
 
@@ -258,40 +276,65 @@ int read_handler( char* box_char, int sock )
     // wipe that mailbox out!
     MAILBOX[box_num][0] = '\0';
 
+    // now that we're done here, we can unlock this mailbox
+    pthread_mutex_unlock( &MAILBOX_LOCK[box_num] );
+
+    printf( "Finished reading!\n" );
+
     return 1;
 }
 
-int write_handler( char* msg, char* box_char )
+int write_handler( char* msg, char* box_char, int sock )
 {
+    // the user didn't provide a message. Return unless you want a seggy fault
+    if( msg == NULL )
+    {
+        return 1;
+    }
+
     // the box number it's trying to write to
     int box_num = -1;
-    
+ 
+    // error statement for when the user provides an invalid mailbox number   
+    char* num_error = "Invalid mailbox number.\n";
+
     // if the box number passed in isn't a number at all... idiots.
     if( !isInt( box_char ) )
     {
-        printf( "Invalid mailbox number!\n" );
+        //printf( "Invalid mailbox number!\n" );
+        write( sock, num_error, strlen( num_error ) );
         return 1;
     }
 
     // convert it to an int
     box_num = atoi( box_char );
 
+    // convert for csc friendly array accessing :)
+    box_num = box_num - 1;
+
     // make sure the mailbox they're looking for is actually a mailbox
-    if( box_num < 0 || box_num > NUM_MAILBOX )
+    if( box_num < 0 || box_num >= NUM_MAILBOX )
     {
-        printf( "Mailbox does not exist!\n" );
+        write( sock, num_error, strlen( num_error ) );       
         return 1;
     }
 
+    // lock this mailbox with its pthread mutex
+    pthread_mutex_lock( &MAILBOX_LOCK[box_num] );
+
     // write the message into that mailbox
     MAILBOX[box_num] = msg;
+
+    // now that we're done, we can unlock it
+    pthread_mutex_unlock( &MAILBOX_LOCK[box_num] );
+
+    printf( "Finished writing!\n" );
 
     return 1;
 }
 
 char* remove_newline( char* line )
 {
-    printf( "checking int\n" );
     size_t len = strlen( line );
     if( len > 0 && line[len-1] == '\n' )
     {
